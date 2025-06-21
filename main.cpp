21fb2b420251bea2581a8f3773ff8017446c8b3c
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-21 22:32:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 #include <fstream>
 #include <cstdint>
 
-//Define circle parameters
+//Define circle parameters.
 #define CIRCLE_RADIUS 1.0
 #define X_CENTER 2.0 
 #define Y_CENTER 2.0
 
-//Define square paramaters
+// Width of the square region where random points are selected from.
+#define REGION_WIDTH 6.0
+
+//Define square parameters.
 #define MAX_X 5.0
 #define MAX_Y 5.0
 
@@ -20,8 +23,9 @@
 //Maximum number of iteration = 4,294,967,295
 #define ITERATIONS 100000000
 
-//Function to check whether randomly picked co-ordinate is inside the circle.
+//Function to check whether a randomly picked point is inside the circle.
 bool insideCircle(double x, double y) {
+	// Returns true if the point (x, y) is inside the circle.
 	if (pow((x - X_CENTER), 2) + pow((y - Y_CENTER), 2) <= pow(CIRCLE_RADIUS, 2)) {
 		return true;
 	}
@@ -30,8 +34,9 @@ bool insideCircle(double x, double y) {
 	}
 }
 
-//Function to check whether randomly picked co-ordinate is inside the square.
+//Function to check whether a randomly picked point is inside the square.
 bool insideSquare(double x, double y) {
+	// Returns true if the point (x, y) is inside the square.
 	if (x >= MIN_X && x <= MAX_X) {
 		if (y >= MIN_X && y <= MAX_Y) {
 			return true;
@@ -41,26 +46,32 @@ bool insideSquare(double x, double y) {
 }
 
 int main() {
+	// Counters for points that fall inside the circle and square.
 	int circle = 0;
 	int square = 0;
-
+        //Open a file to log the number of points within the circle and square. 
 	std::ofstream outputFile("data.csv");
 	outputFile << "Circle,square," << std::endl;
 
+	// Seed the random number generator with the current time for better randomness.
 	srand(time(0));
 	for (uint32_t i = 0; i < ITERATIONS; i++) {
-		double x = static_cast<double>(rand()) / RAND_MAX * 6.0;
-		double y = static_cast<double>(rand()) / RAND_MAX * 6.0;
-		//std::cout << i + 1 << " " << x << ',' << y << std::endl;
+		//Generate random numbers within the region.
+		double x = static_cast<double>(rand()) / RAND_MAX * REGION_WIDTH;
+		double y = static_cast<double>(rand()) / RAND_MAX * REGION_WIDTH;
+		
+		//Check whether the point is within the circle or square.
 		if (insideCircle(x, y)) {
 			circle += 1;
 		}
 		if (insideSquare(x, y)) {
 			square += 1;
 		}
-		outputFile << i << ',' << circle << ',' << square << ',' << std::endl;
+		outputFile << i + 1 << ',' << circle << ',' << square << ',' << std::endl;
 	}
 	std::cout << std::endl << "Monte Carlo simulation complete!" << std::endl;
+	
+	//Estimate the value of Pi and print it out to the terminal.
 	double MY_PI = static_cast<double>(circle) / static_cast<double>(square);
 	std::cout << MY_PI;
 	outputFile.close();
