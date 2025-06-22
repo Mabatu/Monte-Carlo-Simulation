1b31fa0d4c2bc7458aa8cf02db352a25126d94c2
Ezra Mabatu <ezramabatu@gmail.com>
2025-06-22 23:25:50 +0300

    Update error_plot.R

--- Data_Visualization_and_analysis/Error_Analysis/error_plot.R
+++ Data_Visualization_and_analysis/Error_Analysis/error_plot.R
@@ -1,23 +1,22 @@
-# Install ggplot2 only once, you can skip it after first run
+# Install ggplot2 only once; you can skip it after the first run
 # install.packages("ggplot2")
 library(ggplot2)
 library("ggplot2")
 
-myPi.data <- read.csv(file="C:\\Users\\user\\Desktop\\R\\10000000_iterations.csv")
+#Choose the CSV file with the greatest iterations.
+myPi.data <- read.csv(file="Path\\To\\outFile.csv")
 summary(myPi.data)
 head(myPi.data)
-p1 <- ggplot() + geom_line(aes(y = pi-Estimate.Pi, x = ITERATION),
+p1 <- ggplot() + geom_line(aes(y = abs(pi-Estimate.Pi), x = ITERATION),
                            color="red", 
                            linewidth=1, 
                            linetype=1,
                            data = myPi.data) +                    
   geom_hline(yintercept = 0, color = "blue", linetype = 1, size = 1) +
   labs(
-    title = "Estimate of Pi vs iterations",
+    title = "ERROR vs ITERATIONS",
     x = "ITERATIONS",
     y = "ERROR",
     caption = "Horizontal line at 0"
   )
-
 p1
-
